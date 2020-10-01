1308ab8ba35745a7b1d35fc527eb754ad158650c
uditswaroopa <uditswaroopa@icloud.com>
2020-10-01 16:17:59 +0530

    initial Commit

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# AppDevelopmentCollection
+# Scientific CALC
+**SDLC MODEL USED**
+	- AGILE SDLC MODEL
 
-App Development is an important part of the future, with more and more companies coming into the technological picture. It is important that they have their representations on common media such as the Smartphone, one way to do this is to implement Apps.
+**DESCRPTION**
+A simple yet elegant Scientific Calculator App just make those mathematical calculation easier and fun to do! from log to XOR operation, from decimal to Binary to Decimal to Hexadecimal we got you covered with most widely used calculations by programmers. In case you think we misssed something. Do let us know!  
+ 
+ **SCREENSHOTS**
+ 
+ <img width="300"  height = "533" src="https://github.com/uditswaroopa/bluebirds/blob/APP3/screenshots/ui.jpg"></img>
 
-To contribute please checkout the CONTRIBUTING.md
+**EXTERNAL PACKAGE USED**
+
+ - math_expression
---
+++ lib/button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String s;
+  final Function function;
+  Button(this.s, this.function);
+
+//width = 740 - 16 = 624/4 = 131
+  @override
+  Widget build(BuildContext context) {
+    double breadth = (MediaQuery.of(context).size.width - 45) / 4;
+    return Center(
+      child: Container(
+        width: s == '=' ? breadth * 2 + 9 : breadth,
+        height: 40,
+        margin: EdgeInsets.fromLTRB(9, 9, 0, 0),
+        child: RaisedButton(
+          onPressed: () {
+            function(s);
+          },
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 4.0,
+          child: Text(s,
+            style: TextStyle(
+              color:Colors.white,
+              fontSize: 15,
+              fontWeight: FontWeight.w800
+            ),
+          ),
+          color: s == '=' ? Colors.redAccent[400] : Colors.grey[850],
+        ),
+      ),
+    );
+  }
+}
